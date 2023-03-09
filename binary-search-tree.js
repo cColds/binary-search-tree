291d6db9ba577d2110c025edf987f364fb1f5109
cColds <103373668+cColds@users.noreply.github.com>
2023-03-09 17:01:02 -0600

    Move buildTree and prettyPrint functions to Tree class as methods

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -8,41 +8,40 @@ class Node {
 
 class Tree {
 	constructor(array) {
-		this.root = buildTree(array);
+		this.root = this.buildTree(array);
+	}
+
+	buildTree(array) {
+		const tree = [...new Set(array.sort())];
+
+		if (0 > tree.length - 1) return null;
+
+		const mid = Math.floor((tree.length - 1) / 2);
+		const root = new Node(tree[mid]);
+		root.left = this.buildTree(tree.slice(0, mid));
+		root.right = this.buildTree(tree.slice(mid + 1, tree.length));
+
+		return root;
+	}
+
+	prettyPrint(node, prefix = "", isLeft = true) {
+		if (node.right !== null) {
+			this.prettyPrint(
+				node.right,
+				`${prefix}${isLeft ? "│   " : "    "}`,
+				false
+			);
+		}
+		console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.value}`);
+		if (node.left !== null) {
+			this.prettyPrint(
+				node.left,
+				`${prefix}${isLeft ? "    " : "│   "}`,
+				true
+			);
+		}
 	}
 }
-
-// Step 1: start > end, return null
-
-// Step 2: otherwise, get mid
-// Step 3: create a new Node with array[mid] to get root of the subarray
-// Step 4: set left root to recursively call buildTree.
-// Step 5: set right root to recursively call buildTree
-// Step 6: return array[mid] to get mid back to Tree
-// repeat all steps, or stop when step 1 is true
-
-function buildTree(array) {
-	const tree = [...new Set(array.sort())];
-
-	if (0 > tree.length - 1) return null;
-
-	const mid = Math.floor((tree.length - 1) / 2);
-	const root = new Node(tree[mid]);
-	root.left = buildTree(tree.slice(0, mid));
-	root.right = buildTree(tree.slice(mid + 1, tree.length));
-
-	return root;
-}
-
-const prettyPrint = (node, prefix = "", isLeft = true) => {
-	if (node.right !== null) {
-		prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
-	}
-	console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.value}`);
-	if (node.left !== null) {
-		prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
-	}
-};
 
 let binaryTree = new Tree([1, 2, 3, 4, 5, 6, 7]);
-prettyPrint(binaryTree.root);
+binaryTree.prettyPrint(binaryTree.root);
