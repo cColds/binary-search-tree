f20c1c4354f4e36dc0fed4707dc4b865ad36ad70
cColds <103373668+cColds@users.noreply.github.com>
2023-03-08 18:30:07 -0600

    Remove duplicates and sort array in buildTree

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -7,5 +7,33 @@ class Node {
 }
 
 class Tree {
-	constructor(root) {}
+	constructor(array) {
+		this.root = buildTree(array);
+	}
 }
+
+// Step 1: If left and right node are null (leaf nodes) / start > end, return null
+
+// Step 2: otherwise, get mid
+// Step 3: create a new Node with array[mid] to get root of the subarray
+// Step 4: set left root to recursively call buildTree.
+// Step 5: set right root to recursively call buildTree
+// Step 6: return array[mid] to get mid back to Tree
+// repeat all steps, or stop when step 1 is true
+
+function buildTree(array) {
+	const tree = new Set(array.sort());
+
+	if (0 > tree.length - 1) return null;
+	console.log(tree);
+	const mid = Math.floor(1 + (tree.length - 1) / 2);
+	const root = new Node(tree[mid]);
+
+	root.left = buildTree(tree.slice(0, mid));
+	root.right = buildTree(tree.slice(mid, tree.length));
+
+	return root;
+}
+
+let jim = new Tree([1, 2, 3, 4]);
+console.log(jim);
